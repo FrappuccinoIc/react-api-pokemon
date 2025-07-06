a31f38428a454495a6a3b97f50da1c82c79a4778
Frappuccinoic <arseeicore@gmail.com>
2025-07-06 18:31:06 -0400

    Sistema cambiado a un buscador por nombre

--- src/PokemonFetcher.jsx
+++ src/PokemonFetcher.jsx
@@ -3,45 +3,18 @@ import './PokemonFetcher.css';
 
 function PokemonFetcher() {
     const [pokemon, setPokemon] = useState([]);
-    const [cargando, setCargando] = useState(true);
+    const [inputValue, setInput] = useState("");
+    const [cargando, setCargando] = useState(false);
     const [error, setError] = useState(null);
 
-    useEffect(() => {
-        const fetchPokemon = async () => {
-            try {
-                setCargando(true);
-                setError(null);
-                const fetchedPokemon = [];
-                const pokemonId = new Set(); // Estructura de datos usada para evitar registros duplicados
+    const registraCambioValor = (e) => {
+        setInput(e.target.value);
+        e.target.setCustomValidity('') //Reinicia mensaje de validez cuando usuario escribe, ya que no se sabe si es valido aún o no mientras escribe.
+    }
 
-                while(pokemonId.size < 4) {
-                    const randomId = Math.floor(Math.random() * 898) + 1;
-                    pokemonId.add(randomId)
-                }
-
-                const idArray = Array.from(pokemonId);
-
-                for (const id of idArray) {
-                    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-                    if(!response.ok) {
-                        throw new Error(`Error fetching Pokemon with ID ${id}`);
-                    }
-                    const data = await response.json()
-                    fetchedPokemon.push({
-                        id: data.id,
-                        name: data.name,
-                        image: data.sprites.front_default,
-                        types: data.types.map(infoTipo => infoTipo.type.name.charAt(0).toUpperCase() + infoTipo.type.name.slice(1)).join(', ')
-                    })
-                }
-
-                setPokemon(fetchedPokemon);
-
-            } catch(err) {setError(err.message);
-            } finally {setCargando(false);}
-        }
-        fetchPokemon();
-    }, []);
+    /* useEffect(() => {
+        
+    }, []); */
 
     if(cargando) {
         return <p>Cargando Pokémon...</p>;
@@ -50,9 +23,37 @@ function PokemonFetcher() {
         return <p>Error: {error}</p>;
     }
 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            setCargando(true);
+            setError(null);
+
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${inputValue}`);
+            if(!response.ok) {
+                throw new Error(`Error fetching Pokemon with name: ${inputValue}`);
+            }
+            const data = await response.json()
+            const fetchedPokemon = {
+                id: data.id,
+                name: data.name,
+                image: data.sprites.front_default,
+                types: data.types.map(infoTipo => infoTipo.type.name.charAt(0).toUpperCase() + infoTipo.type.name.slice(1)).join(', ')
+            }
+
+            setPokemon([fetchedPokemon]);
+
+        } catch(err) {setError(err.message);
+        } finally {setCargando(false);}
+    }
+
     return (
         <div className="pokemon-container">
-            <h2>Tus 4 Pokémon Aleatorios</h2>
+            <h2>Busca Pokémon: </h2>
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder="Pokémon" name="query" value={inputValue} onChange={(e) => registraCambioValor(e)} minLength={3} maxLength={30} onInvalid={(e) => e.target.setCustomValidity("Ingrese de 3-30 carácteres.")}/>
+                <button>Buscar</button>
+            </form>
             <div className="pokemon-list">
                 {pokemon.map(pokemon => (
                     <div key={pokemon.id} className="pokemon-card">
